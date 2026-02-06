136952617444c61efa2b08ec6b8c118c180edcf0
kuncor3 <60721326+kuncor3@users.noreply.github.com>
2026-02-06 23:26:53 +0700

    Quote file variable in sed command

--- patches.sh
+++ patches.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-find scripts tools -type f | while read file; do sed -Ei "s/\/usr\/bin\/python$/\/usr\/bin\/python2/g" $file; done
+find scripts tools -type f | while read file; do sed -Ei "s/\/usr\/bin\/python$/\/usr\/bin\/python2/g" "$file"; done
