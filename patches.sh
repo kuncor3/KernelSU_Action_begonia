fdd0e935128b70d814a844f67b00296b5ee71e2b
kuncor3 <zx.squad@live.com>
2026-02-07 08:34:53 +0700

    update build-kernel.yml

--- patches.sh
+++ patches.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 find scripts tools -type f | while read file; do sed -Ei "s/\/usr\/bin\/python$/\/usr\/bin\/python2/g" "$file"; done
+find scripts/dtc -type f | while read file; do sed -i 's/^YYLTYPE yylloc;/extern YYLTYPE yylloc;/g' "$file"; done
